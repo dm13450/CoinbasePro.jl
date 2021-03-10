20399f0b7ab35b9056584ea2d757293848401712
dm13450 <dean.markwick@talk21.com>
2021-03-10 21:03:17 +0000

    Export functions

--- src/CoinbasePro.jl
+++ src/CoinbasePro.jl
@@ -5,12 +5,15 @@ using JSON
 using Dates
 using DataFrames
 
-
 const ENDPOINT = "https://api.pro.coinbase.com/"
 
 include("trades.jl")
 include("utils.jl")
+include("products.jl")
+include("stats.jl")
+include("ticker.jl")
+include("book.jl")
 
-export trades, lasttrades
+export trades, lasttrades, products, product, stats, ticker
 
 end
