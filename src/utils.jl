b562a55dd1ab519e25a76437666ffc06c1fbc0c1
dm13450 <dean.markwick@talk21.com>
2021-04-10 13:43:35 +0100

    Correct trades page number

--- src/utils.jl
+++ src/utils.jl
@@ -4,5 +4,5 @@ function get_pages(resp)
     headers = Dict(resp.headers)
 
     before, after = parse.(Int64, (get(headers, "Cb-Before", ""), get(headers, "Cb-After", "")))
-    (before, (before+after)/2, after)
+    (before, before + 1, after)
 end
