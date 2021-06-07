739c22408fd5efd599703a1a79dac52fce35d623
dm13450 <dean.markwick@talk21.com>
2021-06-07 21:48:10 +0100

    Format ticker return

--- src/ticker.jl
+++ src/ticker.jl
@@ -1,6 +1,16 @@
 
+```
+Snapshot information for a currency. 
+```
 function ticker(id::String)
   res = HTTP.get(ENDPOINT * "products/$id/ticker")
   rawData = JSON.parse(String(res.body))
-  DataFrame(rawData)
+  df = DataFrame(rawData)
+  df.ask = parse.(Float64, df.ask)
+  df.bid = parse.(Float64, df.bid)
+  df.size = parse.(Float64, df.size)
+  df.volume = parse.(Float64, df.volume) 
+  df.price = parse.(Float64, df.price) 
+  #df.time = DateTime.(chop.(df.time), dateformat"yyyy-mm-ddTHH:MM:SS.ssssss")
+  df
 end
