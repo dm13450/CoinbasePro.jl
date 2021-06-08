8496deb253e1a244690064c1617511cd4d94f900
dm13450 <dean.markwick@talk21.com>
2021-06-08 08:52:28 +0100

    Update trades test

--- test/test_trades.jl
+++ test/test_trades.jl
@@ -5,7 +5,7 @@
     trades = CoinbasePro.trades("BTC-USD")
     @test length(trades) == 2
     @test isa(trades[1], DataFrame)
-    @test length(trades[2]) == 3
+    @test length(trades[2]) == 2
   end
 
 
